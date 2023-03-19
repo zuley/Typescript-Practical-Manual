26cd36e35ee7064e19e169877689b40880378182
zuley <914230482@qq.com>
2023-03-19 21:46:35 +0800

    init

---
+++ .vitepress/config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Typescript 实战手册",
+  description: "Ts工具类型实现、实战用法等",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '工具类型实现详解', link: '/ToolType/index' }
+    ],
+
+    sidebar: {
+      '/ToolType/': [
+        {
+          text: 'ToolType',
+          items: [
+            { text: 'Markdown Examples', link: '/markdown-examples' },
+          ]
+        }
+      ]
+    },
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
---
+++ index.md
@@ -0,0 +1,25 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "Typescript 实战手册"
+  text: "Typescript 相关的工具类型实现、实战用法等"
+  tagline: 工具类型 实战用法
+  actions:
+    - theme: brand
+      text: TS工具类型实现详解
+      link: /ToolType/index
+    - theme: alt
+      text: 关于我
+      link: /about
+
+features:
+  - title: 工具类型实现详解
+    details: 从Pick开始一步步详解各工具类型的实现，帮助快速掌握工具类型，并可以自己实现需要的类型
+  - title: 实用技巧
+    details: 各种日常开发过程中的，实用TS实用技巧
+  - title: 待定
+    details: 我还没想到这里放什么
+---
+
